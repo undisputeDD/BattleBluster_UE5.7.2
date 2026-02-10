54e7fbf7d6e03148d6e2d4af90975727538fcbad
Andrii Kreidun <kreyduna@gmail.com>
2026-02-10 18:47:40 +0200

    creating game mode and setting global default game mode

---
+++ Source/BattleBlaster/BattleBlasterGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleBlasterGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEBLASTER_API ABattleBlasterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
